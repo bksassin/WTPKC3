61a9c38ad55bde6db7910ae224f6c0af7deffd5f
brandon <bksassin@gmail.com>
2023-02-13 18:25:57 -0500

    uploads all files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PokemonCard from './PokemonCard';
+
+const App = () => (
+  <div className='flex justify-center m-24'>
+    < PokemonCard />
+  </div>
+);
+
+export default App;
